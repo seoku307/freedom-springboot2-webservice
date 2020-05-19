f682cb5db121a509d17f0de52427cf673b23a4c7
KimDongSeok <seoku307@naver.com>
2020-05-19 16:37:55 +0900

    200519 마무리no

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -10,11 +10,10 @@ function switch_proxy() {
     echo "> 전환할 Port: $IDLE_PORT"
     echo "> Port 전환"
     echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
-    #1.
-    #2.
+
     CURRENT_PROFILE=$(curl -s http://localhost/profile)
     echo "> 엔진엑스 Current Profile : $CURRENT_PROFILE"
 
     echo "> 엔진엑스 Reload"
-    sudo service nginx reload #3.
+    sudo service nginx reload
 }
